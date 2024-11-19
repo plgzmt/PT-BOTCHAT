5e8e7b354af5f411f32f1846afbc2f7640ddcd5b
snotesixt <shidoanduni@gmail.com>
2024-11-19 15:15:57 +0700

    fix keyword

--- keywords.py
+++ keywords.py
@@ -2,6 +2,6 @@ KEYWORDS = [
     "ăn", "gym", "uống", "thể hình", "vóc dáng", "dinh dưỡng", "bài tập", "tập luyện",
     "tay", "vai", "ngực", "bụng", "chân", "mông", "bữa ăn", "calo", "đồ ăn", "bữa ăn", 
     "luyện tập", "thon gọn", "giảm cân", "tăng cân", "Giảm mỡ bụng", "Cardio", "mỡ", 
-    "giảm", "cân nặng", "cân đối", "tập", "tập luyện", "cơ", "cơ bắp", "buổi tối", "buổi sáng", "buổi trưa","buổi","cơm","thức ăn","trứng", "bữa","thực đơn"
+    "giảm", "cân nặng", "cân đối", "tập", "tập luyện", "cơ", "cơ bắp", "buổi tối", "buổi sáng", "buổi trưa","buổi","cơm","thức ăn","trứng", "bữa","thực đơn", "nặng","cân","nhẹ","cao","tuổi"
 ]
 #v1.4.6
